c5a48375f2dfef34af739f9d5b3a5c77d29eed37
Tiz52 <carls.mrz@gmail.com>
2022-02-05 12:21:35 -0500

    2.0

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hola Mundo!');
+const express = require ('express');
+require('dotenv').config();
+
+//Creamos el servidor
+const app = express();
+
+//Directorio Público
+app.use( express.static('public') );
+
+//Rutas
+app.use('/api/auth', require('./routes/auth'));
+
+app.listen( process.env.PORT, () => {
+	console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
+} )
---
+++ routes/auth.js
@@ -0,0 +1,17 @@
+/*
+	Rutas de Usuarios / Auth
+	host +/api/auth
+*/
+
+const {Router} = require('express');
+const router = Router();
+
+const {createUser, loginUser, renewToken} = require('../controllers/auth');
+
+router.post('/new', createUser);
+
+router.post('/', loginUser)
+
+router.get('/renew', renewToken)
+
+module.exports = router;
